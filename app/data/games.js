12bdbc86f3c02f0e4c487032c10d4295683a4a99
totallynotkate <katekocijevska@gmail.com>
2017-09-30 20:40:15 +0300

    Add game handler

--- app/data/games.js
+++ app/data/games.js
@@ -1,1 +1,18 @@
-module.exports = [];
+module.exports = [
+    {
+        id: 1,
+        totalPlayers: 6,
+        teamPlayers: [1, 2, 3],
+        calculated: false,
+        price: 300,
+        name: "Game 1"
+    },
+    {
+        id: 2,
+        totalPlayers: 6,
+        teamPlayers: [1, 2, 3],
+        calculated: true,
+        price: 300,
+        name: "Game 2"
+    }
+];
--- app/routes/games.js
+++ app/routes/games.js
@@ -1,14 +1,28 @@
 var games = require('../data/games');
+var _ = require('lodash')
 
 function gameRoutes(app) {
-    app.route('/game')
+    app.route('/games')
         .get(function (req, res) {
-            res.send(games);
+            var {calculated, totalPlayers} = req.query;
+            calculated = calculated !== undefined ? JSON.parse(calculated) : undefined;
+            totalPlayers = totalPlayers !== undefined ? JSON.parse(totalPlayers) : undefined;
+            res.send(_.filter(games, _.omitBy({calculated, totalPlayers}, _.isUndefined)));
         })
         .post(function (req, res) {
             games.push(req.body);
             res.send(games);
         });
 }
+
+/**
+ game = {
+    "totalPlayers": int,
+    "teamPlayers": [ids],
+    "calculated": bool,
+    "price": int,
+    "name": string
+}
+ */
 
 module.exports = gameRoutes;
